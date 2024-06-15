41e9c0d202ff2c2b886fc64d367fcdcb33330d9d
ogabriel <gabrieloliver8991@gmail>
2024-06-15 09:04:26 -0300

    update colors

--- colors.sh
+++ colors.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "\033[37;44m This wont work"
+
 echo -e "\033[37;44m White text on a blue background!\033[0m"
 echo -e "\033[32m This is green text!"
 echo -e "\033[31m This is red text!"
