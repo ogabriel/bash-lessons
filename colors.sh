65c4d8db6cfa3bcdf74cf80e4375388e3f57e0a1
ogabriel <gabrieloliver8991@gmail>
2024-06-15 09:03:44 -0300

    colors

---
+++ colors.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo -e "\033[37;44m White text on a blue background!\033[0m"
+echo -e "\033[32m This is green text!"
+echo -e "\033[31m This is red text!"
+
+echo -e "\033[0m This is black text!"
+echo -e "\033[31m This is red text!"
+echo -e "\033[32m This is green text!"
