791b0399f450d752a4e13cfa4ca27378832060d5
ogabriel <gabrieloliver8991@gmail>
2024-06-15 09:03:30 -0300

    arrays

---
+++ arrays.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+array=()
+
+array+=(one two)
+array+=(three)
+array+=("four five")
+
+function print_array() {
+    echo "count: ${#array[@]}"
+
+    for i in "${!array[@]}"; do
+        echo "$i: ${array[$i]}"
+    done
+}
+
+print_array
+
+unset array[1]
+
+print_array
